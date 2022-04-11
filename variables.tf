47594ec157c2463ba56cce59a8beaa97c815986d
Ivan Sinyagovskiy <vaneuk@gmail.com>
2022-04-11 13:03:56 +0300

    updated variable validation

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "value" {
   type        = string
 
   validation {
-    condition     = "auto" == var.value || can(regex("\\d+\\.\\d+\\.\\d+\\.\\d+:\\d+", var.value)) || can(regex("\\d+:\\d+", var.value)) || false
+    condition     = "auto" == var.value || can(regex("\\d+\\.\\d+\\.\\d+\\.\\d+:\\d+", var.value)) || can(regex("\\d+:\\d+", var.value))
     error_message = "Allowed formats: `auto`, `1.1.1.1:1`, `65535:1`."
   }
 }
